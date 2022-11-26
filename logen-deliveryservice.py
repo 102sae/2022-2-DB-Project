ac9bd5edb50fc0aca69a5b841b3b5b02a0572d21
102sae <95170874+102sae@users.noreply.github.com>
2022-11-27 02:19:00 +0900

    reservation complete

--- logen-deliveryservice.py
+++ logen-deliveryservice.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter.messagebox as MessageBox
 import mysql.connector as mysql
+from random import *
 
 try:
     import Tkinter as tk
@@ -10,29 +11,38 @@ except:
 #자신 localhost에 맞게 수정  
 con = mysql.connect(user='root', password='@mysql0056', host='127.0.0.1',port="3307", database='dgu')  
 
-def logincheck(e_name, e_phone, e_mail):
+def isUser(s_name, s_phone, s_email):
     cursor = con.cursor(buffered=True)
-    cursor.execute("select count(*) from users where user_name='"+ e_name.get() + "' and user_phone='"+e_phone.get() + "' and user_email='"+e_mail.get()+"'")
-    rows = cursor.fetchall()
+    cursor.execute("select count(*) from users where user_name='"+ s_name.get()+ "' and user_phone='"+s_phone.get() + "' and user_email='"+s_email.get()+"'")
+    row = cursor.fetchall()
+    print(row[0][0])
+    if(row[0][0]):
+        return True
+    else:
+        return False
 
-    for row in rows:
-        if(row[0]):
-            MessageBox.showinfo("Login Status", "Login Successful")
-            app.switch_frame(ReservationReceiver)
-            print("Logged IN Successfully")
-        else:
-            MessageBox.showinfo("Login Status", "Login Failed")
-            e_name.delete(0, 'end')
-            e_phone.delete(0, 'end')
-            e_mail.delete(0, 'end')
-            
+
+def logincheck(e_name, e_phone, e_mail):
+    is_user = isUser(e_name, e_phone, e_mail)
+    if(is_user == True):
+        MessageBox.showinfo("Login Status", "Login Successful")
+        app.switch_frame(ReservationReceiver)
+        print("Logged IN Successfully")
+    else:
+        MessageBox.showinfo("Login Status", "Login Failed")
+        e_name.delete(0, 'end')
+        e_phone.delete(0, 'end')
+        e_mail.delete(0, 'end')
+
+
 def senderInfo(sender_name,sender_phonenum,sender_addr,sender_email,sender_visit):
     s_name = sender_name.get()
     s_phone = sender_phonenum.get()
     s_addr = sender_addr.get("1.0","end-1c")
     s_email = sender_email.get()
     s_visit = sender_visit.get()
-    enrollSender(s_name,s_phone,s_addr,s_email)
+    random_num = randint(10000000, 99999999)
+    enrollSender(s_name,s_phone,s_addr,s_email, s_visit, random_num)
     app.switch_frame(Complete)
     
 
@@ -63,9 +73,7 @@ def enrollReceiver(r_name,r_phone,r_addr):
     cursor.execute("select count(*) from receiver")
     rows = cursor.fetchall()
     r_id = rows[0][0] + 101 #다음 receiver id
-
-    #insert_val =  (s_id, r_id, p_id, s_name, s_phone, s_addr, s_email)
-    #insert_sql = "INSERT INTO sender (sender_id, receiver_id, product_id, sender_name, sender_phone, sender_address, sender_email) VALUES (%d, %d, %d, %s, %s, %s, %s)" % tuple(insert_val)
+    #insert receiver table
     cursor.execute("INSERT INTO receiver (receiver_id, receiver_name, receiver_phone, receiver_address) VALUES (" + "'"+str(r_id)+"'," + "'"+r_name+"',"+ "'"+r_phone+"'," + "'"+r_addr+"'" +")")
     con.commit()
 
@@ -74,14 +82,15 @@ def enrollProduct(p_name,p_weight,p_size,p_cost,p_quantity):
     cursor.execute("select count(*) from product")
     rows = cursor.fetchall()
     p_id = rows[0][0] + 1001 #다음 product id 
+    #insert productr table
     cursor.execute("INSERT INTO product (product_id, product_name, product_weight, product_size, product_cost, product_quantity) VALUES (" + "'"+str(p_id)+"'," + "'"+p_name+"',"+ "'"+p_weight+"'," + "'"+p_size+"'," + "'"+p_cost+"'," + "'"+p_quantity+"'" +")")
     con.commit()
 
-def enrollSender(s_name, s_phone, s_addr, s_email):
+def enrollSender(s_name, s_phone, s_addr, s_email, s_visit, random_num):
     cursor = con.cursor(prepared=True)
     cursor.execute("select count(*) from product")
     rows = cursor.fetchall()
-    p_id = rows[0][0] + 1000 #다음 product id 
+    p_id = rows[0][0] + 1000 #현재 product id 
 
     cursor.execute("select count(*) from sender")
     rows = cursor.fetchall()
@@ -89,14 +98,49 @@ def enrollSender(s_name, s_phone, s_addr, s_email):
 
     cursor.execute("select count(*) from receiver")
     rows = cursor.fetchall()
-    r_id = rows[0][0] + 100#다음 receiver id
-   
+    r_id = rows[0][0] + 100#현재 receiver id
+    
+    cursor.execute("select count(*) from reservation")
+    rows = cursor.fetchall()
+    re_id = rows[0][0] + 1#현재 reservation id
+    
+    u_id = 1
+
+   #insert sender table
     cursor.execute("INSERT INTO sender (sender_id, receiver_id, product_id, sender_name, sender_phone, sender_address, sender_email) VALUES (" + "'"+str(s_id)+"'," + "'"+str(r_id)+"',"+ "'"+str(p_id)+"'," + "'"+s_name+"'," + "'"+s_phone+"'," + "'"+s_addr+"'," + "'"+s_email+"'" +")")
     con.commit()
 
-def getSenderAddress():
-    pass
-             
+    #insert reservation table
+    cursor.execute("INSERT INTO reservation (reservation_id, user_id, sender_id, receiver_id, product_id, visiting_date, reservation_number) VALUES (" + "'"+str(re_id)+"'," + "'"+str(u_id)+"',"+ "'"+str(s_id)+"'," + "'"+str(r_id)+"'," + "'"+str(p_id)+"'," + "'"+s_visit+"'," + "'"+str(random_num)+"'" +")")
+    con.commit()
+
+    cursor.execute("select * from reservation where reservation_number='"+str(random_num)+"'")
+    rows = cursor.fetchall()
+    global reservation_num
+    reservation_num=rows[0][6]
+    print(rows[0][6])
+
+    cursor.execute("select sender_name from sender where sender_id='"+str(s_id)+"'")
+    rows = cursor.fetchall()
+    global sender_name
+    sender_name = rows[0][0]
+
+    cursor.execute("select receiver_name from receiver where receiver_id='"+str(r_id)+"'")
+    rows = cursor.fetchall()
+    global receiver_name
+    receiver_name = rows[0][0]
+
+    cursor.execute("select product_quantity from product where product_id='"+str(p_id)+"'")
+    rows = cursor.fetchall()
+    global product_quantity
+    product_quantity = rows[0][0]
+
+     #visidate
+    cursor.execute("select visiting_date from reservation where reservation_id='"+str(re_id)+"'")
+    rows = cursor.fetchall()
+    global visiting_date
+    visiting_date = rows[0][0]
+  
 
 class SampleApp(tk.Tk):
     def __init__(self):
@@ -169,10 +213,8 @@ class ReservationSender(tk.Frame):
         sender_email.grid(row=4, column=1, pady=10, padx=20)
         sender_visit =  Entry(self,width=40)
         sender_visit.grid(row=5, column=1, pady=10, padx=20)
-        #get sender address btn
-        addr_btn = Button(self, width=15, text='address book', font=('Times', 14), bg="white", command=lambda:getSenderAddress()).grid(row=3,column=3,padx=20);
         #next page btn + sender Info save
-        reg_btn = Button(self, width=15, text='next', font=('Times', 14), bg="white",
+        reg_btn = Button(self, width=15, text='submit', font=('Times', 14), bg="white",
                          command=lambda:senderInfo(sender_name,sender_phonenum,sender_addr,sender_email,sender_visit)).grid(row=6, column=3, pady=10, padx=10)
 
 class ReservationReceiver(tk.Frame):
@@ -192,7 +234,7 @@ class ReservationReceiver(tk.Frame):
         receiver_addr.grid(row=3, column=1, pady=10, padx=20)
         #input btn
         #next page btn + receiver Info save
-        reg_btn = Button(self, width=15, text='next', font=('Times', 14), bg="white",
+        reg_btn = Button(self, width=15, text='Next', font=('Times', 14), bg="white",
                          command=lambda:receiverInfo(receiver_name,receiver_phonenum,receiver_addr)).grid(row=6, column=3, pady=10, padx=10)
 
 
@@ -218,17 +260,23 @@ class ReservationProduct(tk.Frame):
         product_size.grid(row=4, column=1, pady=10, padx=20)
         product_weight =  Entry(self,width=40)
         product_weight.grid(row=5, column=1, pady=10, padx=20)
-        #input btn
+
         #next page btn + product Info save
-        reg_btn = Button(self, width=15, text='submit', font=('Times', 14), bg="white",
+        reg_btn = Button(self, width=15, text='Next', font=('Times', 14), bg="white",
                          command=lambda:productInfo(product_name,product_quantity ,product_cost ,product_size,product_weight)).grid(row=6, column=3, pady=10, padx=10)
 
+#reservatioin complete
 class Complete(tk.Frame):
     def __init__(self,master):
         tk.Frame.__init__(self,master)
-        tk.Label(self, text="Reservation Complete", font=('Helvetica', 18, "bold")).grid(row=0, column=1, pady=10,padx=10)
+        tk.Label(self, text="Reservation Complete", font=('Helvetica', 18, "bold")).grid(row=0, column=0, pady=10,padx=10)
+        Label(self, text="reservation id : "+ str(reservation_num),font=('Helvetica', 14)).grid(row=1, column=0, sticky=W, pady=10,padx=10)
+        Label(self, text="sender name : "+ str(sender_name),font=('Helvetica', 14)).grid(row=2, column=0, sticky=W, pady=10,padx=10)
+        Label(self, text="receiver name : "+ str(receiver_name),font=('Helvetica', 14)).grid(row=3, column=0, sticky=W, pady=10,padx=10)
+        Label(self, text="product quantity : "+ str(product_quantity),font=('Helvetica', 14)).grid(row=4, column=0, sticky=W, pady=10,padx=10)
+        Label(self, text="visiting date : "+ str(visiting_date),font=('Helvetica', 14)).grid(row=5, column=0, sticky=W, pady=10,padx=10)
 
-#실행  
+#Run  
 if __name__ == "__main__":
     app = SampleApp()
     app.title("LOGEN")
